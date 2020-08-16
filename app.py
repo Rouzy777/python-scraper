6339cbffdef92616438631b9eab5ce59db14dfc4
Alexander <kharchenko7717@gmail.com>
2020-08-16 23:17:28 +0300

    Update app.py

--- app.py
+++ app.py
@@ -21,4 +21,4 @@ def index():                      # call method hello
     return render_template('index.html')         # which returns "hello world"
 
 if __name__ == "__main__":        # on running python app.py
-    app.run()                     # run the flask app
+    app.run(debug=True,host='0.0.0.0', port=5000)                     # run the flask app
