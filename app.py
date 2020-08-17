7d7d3950bd9005030f55d58cc4c4c57f7de18b51
Alexander <kharchenko7717@gmail.com>
2020-08-17 12:11:48 +0300

    remove cors

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
 # app.py
 
 from flask import Flask, render_template, request, json         # import flask
-from flask_cors import CORS
 import scrapper_amaz
 
 app = Flask(__name__)             # create an app instance
-CORS(app)
 
 @app.route("/", methods = ['GET', 'POST'])                   # at the end point /
 def index():                      # call method hello
